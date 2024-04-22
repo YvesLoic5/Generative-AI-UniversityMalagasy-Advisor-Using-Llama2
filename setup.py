e5ccf07a100c41fe840bfdfca3028ef1d889023a
yvesloic5 <yvesloic58@gmail.com>
2024-04-22 17:03:20 +0300

    added src files and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= "0.0.0",
+    author= 'SOATIANAINA Yves Loïc',
+    author_email= 'yvesloic58@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
